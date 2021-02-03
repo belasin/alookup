f30b950e52e9db7e252c4abf939e597178976998
Justin Unwin <justinunwin@jabbamobile.co.za>
2021-02-03 11:45:07 +0200

    Ok finally got a chance to work on this, making some progress  - Added some normalisation to the request data  - Used the normalisation to format the request for google  - Started adding the inline documents  - Added the hook to handle the caching lookup (may change this to a decorator as it may be easier)

--- alookup/lib/glookup.py
+++ alookup/lib/glookup.py
@@ -2,8 +2,60 @@
 Google Lookup System
 """
 
-def normalise_request(request_payload):
-	pass
+class InvalidAPRequest(Exception):
+    pass
 
-def perform_lookup(request_payload):
+def lookup_cache(apscan):
+    pass
+
+def format_ap_entry(ap_entry):
+    """Processes the Access Point dictionary and produce a stripped down entry for use in the request dataset
+    
+    Args:
+        ap_entry (DICT): Access Point dict entry
+    """
+    return dict(
+        macAddress = ap_entry["bssid"],
+        signalStrength = int(ap_entry["rssi"]),
+        age = 0,
+        channel = int(ap_entry["channel"])
+    )
+
+def normalise_request(apscan):
+    """Attempts to locate the access point information and return it as a list of access points
+    
+    Args:
+        apscan (DICT|LIST): Description
+    """
+
+    out = []
+    if type(apscan) == dict:
+        #We need to itterate through the key values and
+        # look for a list of dictionaries where a bssid key exists
+        for k,v in apscan.items():
+            if type(v) == list:
+                out = normalise_request(v)
+                if out:
+                    return out
+    elif type(apscan) == list:
+        for ent in apscan:
+            formatted_ent = format_ap_entry(ent)
+            if formatted_ent:
+                out += [formatted_ent]
+    return out
+
+def perform_lookup(apscan):
+    """Sends a request to google's geolocation service to lookup the location
+    based on the Access Point list provided in the request
+    
+    Args:
+        apscan (DICT|LIST): a list of access points
+    """
+    #We need to start by assuming we may not get the information
+    #In the correct format
+    apscan = normalise_request(apscan)
+
+    if not apscan:
+        raise InvalidAPRequest("Invalid Request")
+	print(repr(apscan))
 	pass
--- alookup/views/default.py
+++ alookup/views/default.py
@@ -1,17 +1,32 @@
 from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPBadRequest
-from ..lib import glookup
+from ..lib.glookup import perform_lookup
+from ..lib.glookup import InvalidAPRequest
 
 
 @view_config(route_name='root', renderer='json')
-def perform_lookup(request):
+def lookup_view(request):
+    """Summary
+    
+    Args:
+        request (pyramid.request): ...
+    
+    Returns:
+        LIST: Description
+    
+    Raises:
+        HTTPBadRequest: On an error in the request, an HTTPBadRequest is raised
+    """
     request_payload = None
     try:
         request_payload = request.json_body
     except ValueError, exc:
         raise HTTPBadRequest(str(exc))
 
-    return glookup.perform_lookup(request_payload)
+    try:
+        return perform_lookup(request_payload)
+    except InvalidAPRequest, exc:
+        raise HTTPBadRequest(str(exc))
 
 def includeme(config):
     config.add_route('root', '/')
