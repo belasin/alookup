2a755a4b91aa473d2a65fc5692cfa2057d7bd25d
Justin Unwin <justinunwin@jabbamobile.co.za>
2021-02-03 16:11:46 +0200

    Started implementation of the Dict Backend

--- alookup/lib/caching.py
+++ alookup/lib/caching.py
@@ -1,15 +1,31 @@
 from functools import update_wrapper
 from pyramid.threadlocal import get_current_registry
 from pyramid.settings import asbool
+import uuid
 import re
 
 class DictBackend(object):
     def __init__(self, cnf):
         self.data = {}
 
+    def flush(self):
+        self.data = {}
+
+    def addkey(self, key, value):
+        raise NotImplemented
+
+    def appendkey(self, key, value):
+        raise NotImplemented
+
+    def getkey(key):
+        raise NotImplemented
+
+    def getkey(key):
+        raise NotImplemented
+
 class RedisBackend(object):
     def __init__(self, cnf):
-        raise NotImplemented("Place Holder for Later")
+        raise NotImplemented("Definately can be done if you wanted to scale the system")
 
 backend_urn_re = re.compile("^(dict|redis\:\/\/(.*){3,5})$")
 
@@ -40,22 +56,44 @@ class GoogleGeocodeCache(object):
         self.func = func
 
     def generate_cache_key(self, ent):
-        k = [i for i in signal_bands.keys() if i >= ent["signalStrength"]][0]
-        sband = signal_bands[k]
+        k = [i for i in self.signal_bands.keys() if i >= ent["signalStrength"]][0]
+        sband = self.signal_bands[k]
         return "%s::%s" % (ent["macAddress"], sband)
 
-    def lookup_cache(self, apscan):
+    def save(self, apscan, result):
         apscan = sorted(apscan, key=lambda x:x["signalStrength"],reverse=True)
+        _id = uuid.uuid4()
+        _keylist = []
+        for ent in apscan:
+            k = self.generate_cache_key(ent)
+            self.backend.appendkey(k, _id)
+            _keylist.append(k)
+        _data = {
+            "request": apscan,
+            "result": result,
+            "keylist": _keylist
+        }
+        self.backend.addkey(_id, _data)
+
+    def lookup(self, apscan):
+        apscan = sorted(apscan, key=lambda x:x["signalStrength"],reverse=True)
+        for ent in apscan:
+            k = self.generate_cache_key(ent)
+            print k
         print self.backend
-        pass
 
     def __call__(self, *a):
+        print self.is_enabled
         if self.is_enabled:
-            res = self.lookup_cache(apscan)
+            res = self.lookup(a[0])
             if res:
                 return res
-        res = self.func(*a)
-        return res
+            else:
+                res = self.func(*a)
+                self.save(a[0], res)
+            raise Exception("GOTHERE")
+        else:
+            return self.func(*a)
 
 
 def setup_cache_fromsettings(settings):
@@ -64,7 +102,7 @@ def setup_cache_fromsettings(settings):
     Args:
         settings (DICT): Pyramid Settings
     """
-    GoogleGeocodeCache.is_enabled = asbool(settings.get("cache.enable"))
+    GoogleGeocodeCache.is_enabled = asbool(settings.get("cache.enabled"))
     GoogleGeocodeCache.depth = int(settings.get("cache.depth", 6))
     GoogleGeocodeCache.match_required = int(settings.get("cache.match_required", 80))
     if backend_urn_re.search(settings.get("cache.backend", "dict")):
