e9c9d1141c53dfa21d63be34ceb6afa97f06c4ef
Justin Unwin <justinunwin@jabbamobile.co.za>
2021-02-03 12:35:16 +0200

    PEP8

--- alookup/lib/glookup.py
+++ alookup/lib/glookup.py
@@ -10,42 +10,63 @@ log = logging.getLogger("google.geolocation")
 
 
 class InvalidConfiguration(Exception):
+
+    """Summary
+    """
+
     pass
 
+
 class InvalidAPRequest(Exception):
+
+    """Summary
+    """
+
     pass
+
 
 def cache(f):
     def cache_wrapper(apscan, settings):
+        """Summary
+
+        Args:
+            apscan (TYPE): Description
+            settings (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         res = f(apscan, settings)
         return res
     return cache_wrapper
 
+
 def format_ap_entry(ap_entry):
     """Processes the Access Point dictionary and produce a stripped down entry for use in the request dataset
-    
+
     Args:
         ap_entry (DICT): Access Point dict entry
     """
     return dict(
-        macAddress = ap_entry["bssid"],
-        signalStrength = int(ap_entry["rssi"]),
-        age = 0,
-        channel = int(ap_entry["channel"])
+        macAddress=ap_entry["bssid"],
+        signalStrength=int(ap_entry["rssi"]),
+        age=0,
+        channel=int(ap_entry["channel"])
     )
+
 
 def normalise_request(apscan):
     """Attempts to locate the access point information and return it as a list of access points
-    
+
     Args:
         apscan (DICT|LIST): Description
     """
 
     out = []
     if type(apscan) == dict:
-        #We need to itterate through the key values and
+        # We need to itterate through the key values and
         # look for a list of dictionaries where a bssid key exists
-        for k,v in apscan.items():
+        for k, v in apscan.items():
             if type(v) == list:
                 out = normalise_request(v)
                 if out:
@@ -57,27 +78,42 @@ def normalise_request(apscan):
                 out += [formatted_ent]
     return out
 
+
 @cache
 def geolocate(apscan, settings={}):
+    """Summary
+
+    Args:
+        apscan (TYPE): Description
+        settings (dict, optional): Description
+
+    Returns:
+        TYPE: Description
+
+    Raises:
+        InvalidConfiguration: Description
+    """
     log.debug("Request for: %s" % apscan)
     api_key = settings.get("geolocate.api_key")
     if not api_key:
-        raise InvalidConfiguration("geolocate.api_key has not been set in the config")
+        raise InvalidConfiguration(
+            "geolocate.api_key has not been set in the config")
     url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + api_key
     _post_info = dict(wifiAccessPoints=apscan)
-    response = requests.post(url,data=json.dumps(_post_info))
+    response = requests.post(url, data=json.dumps(_post_info))
     if response.status_code == 200:
         return response.json()
+
 
 def perform_lookup(apscan, settings=None):
     """Sends a request to google's geolocation service to lookup the location
     based on the Access Point list provided in the request
-    
+
     Args:
         apscan (DICT|LIST): a list of access points
     """
-    #We need to start by assuming we may not get the information
-    #In the correct format
+    # We need to start by assuming we may not get the information
+    # In the correct format
     if not settings:
         settings = get_current_registry().settings
     apscan = normalise_request(apscan)
